15eab2e7d1895f94b78b553a901b871c25c3a280
Michael Mergell <mimergel@microsoft.com>
2024-08-30 15:39:28 +0200

    Create create_student_vms.sh

---
+++ mentor/env-prep/create_student_vms.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Define variables
+# Note: NSG manually created upfront and opened RDP port
+# A master VM saphackday00 is created manually and a snapshot is taken upfront
+
+resourceGroup="DSAG"  # Resource group for the VMs and PIPs
+location="germanywestcentral"
+vmSize="Standard_D4s_v3"
+networkName="DSAG-vnet"
+subnetName="default"
+nsgName="saphackday00485"
+vmPrefix="saphackday"
+diskPrefix="saphackdayosdisk"
+ipPrefix="saphackdaypip"
+snapshotName="saphackday00-disk-snapshot"
+diskPrefix="saphackdayosdisk"
+
+# Loop to create VMs with public IPs from master OS disk snapshot
+# Adjust number of VMs as needed
+for i in $(seq -w 1 30)
+do
+  vmName="${vmPrefix}${i}"
+  diskName="${diskPrefix}${i}"
+  ipName="${ipPrefix}${i}"
+
+  # Create a managed disk from the snapshot
+  az disk create \
+    --resource-group $resourceGroup \
+    --name $diskName \
+    --location $location \
+    --source $snapshotName \
+    --query "id" --output tsv
+
+  echo "Managed disk $diskName created."
+
+  # Create a public IP address
+  az network public-ip create \
+    --resource-group $resourceGroup \
+    --name $ipName \
+    --location $location \
+    --allocation-method Static \
+    --sku Standard
+
+  echo "Public IP $ipName created."
+
+  # Create VM using the managed disk and attach the public IP
+  az vm create \
+    --resource-group $resourceGroup \
+    --name $vmName \
+    --location $location \
+    --size $vmSize \
+    --attach-os-disk $diskName \
+    --os-type Windows \
+    --vnet-name $networkName \
+    --subnet $subnetName \
+    --nsg $nsgName \
+    --public-ip-address $ipName \
+    --no-wait
+
+  echo "VM $vmName creation initiated with Public IP $ipName."
+done
+
+echo "All VM creation requests submitted."
